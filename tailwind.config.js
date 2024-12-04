9f037baed27d926c3b82763a4044157a2d015423
Cecilie <cepe0001@stud.kea.dk>
2024-12-04 11:01:02 +0100

    tilføjet custom farver i tailwind.config.js, fjernet billeder i public og fjernet favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customPink: "#EC2783",
+        customRed: "#D82023",
+        customOrange: "EC4D08",
       },
     },
   },

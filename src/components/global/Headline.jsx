eef5258cd6580d6b5d0ecf37fec960eaeb94f7ab
Cecilie <cepe0001@stud.kea.dk>
2024-12-05 16:35:53 +0100

    fået lavet headline component

--- src/components/global/Headline.jsx
+++ src/components/global/Headline.jsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+
+import { Caesar_Dressing } from "next/font/google";
+
+const ceasarDressing = Caesar_Dressing({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
+
 const Headline = ({ src, text }) => {
   return (
-    <div>
+    <div className="flex gap-2">
       <Image src={src} alt="rune" width={50} height={50} />
-      <h1>{text}</h1>
+      <h1 className={`${ceasarDressing.className} text-6xl`}>{text}</h1>
     </div>
   );
 };

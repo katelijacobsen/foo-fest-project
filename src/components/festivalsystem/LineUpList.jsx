5e68d877d1c5a58b2dd629883ea91199f2ea2e59
Cecilie <cepe0001@stud.kea.dk>
2024-12-06 12:27:01 +0100

    fået lineup feature til at fungere

--- src/components/festivalsystem/LineUpList.jsx
+++ src/components/festivalsystem/LineUpList.jsx
@@ -6,23 +6,17 @@ const LineUpList = ({ initialLineup }) => {
   const [bands, setBands] = useState(initialLineup);
 
   return (
-    <div>
-      <ul className="flex flex-wrap justify-center">
-        {bands.map((band, index) => {
-          // Beregn fontstørrelsen dynamisk baseret på index
-          const fontSize = Math.max(48 - index, 20); // Fontstørrelsen starter ved 48px og går ned til 20px
-          return (
-            <li
-              className="w-[fit] p-1 hover:text-customOrange transition ease-in-out hover:-translate-y-1"
-              key={index}
-              style={{ fontSize: `${fontSize}px` }} // Brug inline styling til font-size
-            >
-              <Link href="#">{band.name}</Link>
+    <section className="max-w-screen-lg mx-auto">
+      <ul className="flex flex-wrap justify-center items-center gap-2">
+        {bands.map((band, index) => (
+          <Link href={`/pages/program/${band.slug}`} key={band.slug}>
+            <li key={band.slug} className={index < 13 ? "text-3xl lg:text-5xl" : index < 40 ? "text-xl lg:text-3xl" : index < 50 ? "text-lg lg:text-xl" : index < 60 ? "text-md lg:text-lg" : index < 70 ? "text-base lg:text-md" : index < 80 ? "text-sm lg:text-md" : "text-xs lg:text-sm"}>
+              {band.name}
             </li>
-          );
-        })}
+          </Link>
+        ))}
       </ul>
-    </div>
+    </section>
   );
 };
 

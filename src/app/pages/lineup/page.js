0f2add3dc4e3e2320d4351cb5a079b373d298605
Cecilie <cepe0001@stud.kea.dk>
2024-12-06 10:52:14 +0100

    udkommenteret lineup page

--- src/app/pages/lineup/page.js
+++ src/app/pages/lineup/page.js
@@ -1,20 +1,20 @@
-import LineUpList from "@/components/festivalsystem/LineUpList";
-import Headline from "@/components/global/Headline";
-import LineUpRune from "@/img/svg/lineup_rune.svg";
+// import LineUpList from "@/components/festivalsystem/LineUpList";
+// import Headline from "@/components/global/Headline";
+// import LineUpRune from "@/img/svg/lineup_rune.svg";
 
-export default async function Page() {
-  const fetchBands = async () => {
-    let response = await fetch("http://localhost:8080/bands");
-    let data = await response.json();
-    return data;
-  };
+// export default async function Page() {
+//   const fetchBands = async () => {
+//     let response = await fetch("http://localhost:8080/bands");
+//     let data = await response.json();
+//     return data;
+//   };
 
-  const bands = await fetchBands();
+//   const bands = await fetchBands();
 
-  return (
-    <div>
-      <Headline src={LineUpRune} text="LINE-UP" />
-      <LineUpList initialLineup={bands} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Headline src={LineUpRune} text="LINE-UP" />
+//       <LineUpList initialLineup={bands} />
+//     </div>
+//   );
+// }
--- src/app/pages/program/page.js
+++ src/app/pages/program/page.js
@@ -1,46 +1,30 @@
 import ProgramList from "@/components/festivalsystem/ProgramList";
 
 export default async function Page() {
+  //fetch datasæt med endpoint /bands
   const fetchBands = async () => {
     let response = await fetch("http://localhost:8080/bands");
     let data = await response.json();
     return data;
   };
 
+  //fetch datasæt med endpoint /schedule
   const fetchSchedule = async () => {
     let response = await fetch("http://localhost:8080/schedule");
     let data = await response.json();
     return data;
   };
 
+  //fetch datasæt med endpoint /events
   const fetchEvents = async () => {
     let response = await fetch("http://localhost:8080/events");
     let data = await response.json();
     return data;
   };
-
+  //variabel for det data, som de tre datasæt retunerer - gør det muligt at bruge data lokalt i funktionen og sende dem videre som props til din App-komponent
   const bands = await fetchBands();
   const schedule = await fetchSchedule();
   const events = await fetchEvents();
-
-  // console.log(bands);
-  // console.log(events);
-
-  //kombiner data (bands og events)
-  // const combinedData = bands.map((band) => {
-  //   const eventInfo = events.find((event) => event.act.act === band.name);
-
-  //   if (eventInfo) {
-  //     return {
-  //       ...band,
-  //       time: `${eventInfo.act.start} - ${eventInfo.act.end}`,
-  //       scene: eventInfo.scene,
-  //       day: eventInfo.day, // Dag
-  //       cancelled: eventInfo.act.cancelled, // Aflyst eller ej
-  //     };
-  //   }
-  //   return band;
-  // });
 
   // Kombiner bands, schedule og events
   const organizedByScene = Object.entries(schedule).reduce((acc, [scene, days]) => {
@@ -78,3 +62,21 @@ export default async function Page() {
 }
 
 // initialBands={bands} initialSchedule={schedule} initialEvents={events}
+// console.log(bands);
+// console.log(events);
+
+//kombiner data (bands og events)
+// const combinedData = bands.map((band) => {
+//   const eventInfo = events.find((event) => event.act.act === band.name);
+
+//   if (eventInfo) {
+//     return {
+//       ...band,
+//       time: `${eventInfo.act.start} - ${eventInfo.act.end}`,
+//       scene: eventInfo.scene,
+//       day: eventInfo.day, // Dag
+//       cancelled: eventInfo.act.cancelled, // Aflyst eller ej
+//     };
+//   }
+//   return band;
+// });

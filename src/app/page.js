deacd4ed70b08d424a907c239a218be807ce4795
”sagecave” <Andreassage@gmail.com>
2024-12-06 13:05:03 +0100

    merge conflit løst

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,7 @@
 import Image from "next/image";
-<<<<<<< HEAD
 import HeroSection from "@/components/festivalsystem/HeroSection";
 import Footer from "@/components/global/Footer";
 
 export default function Home() {
   return <div>{/* <HeroSection /> */}</div>;
-=======
-// import HeroSection from "@/components/HeroSection";
-import Header from "@/components/global/Header";
-
-export default function Home() {
-  return (
-    <div>
-      <Header />
-      {/* <HeroSection /> */}
-    </div>
-  );
->>>>>>> header
 }

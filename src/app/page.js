b9dfbaed8123ca93f9740bbdab3637c7b657cbfa
Cecilie <cepe0001@stud.kea.dk>
2024-12-09 09:18:59 +0100

    tømt homepage for import og indhold

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,3 @@
-import Image from "next/image";
-import HeroSection from "@/components/festivalsystem/HeroSection";
-
 export default function Home() {
-<<<<<<< HEAD
-  return <div>{/* <HeroSection /> */}</div>;
-=======
-  return (
-    <div>
-      {/* <HeroSection /> */}
-      {/* <Button buttonContent="lol"></Button>
-      <MyButton></MyButton> */}
-    </div>
-  );
->>>>>>> singleview-feature
+  return <div></div>;
 }

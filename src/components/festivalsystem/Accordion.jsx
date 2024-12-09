3a35db23b0e516427cedbcc00af6e44c2a928a07
Cecilie <cepe0001@stud.kea.dk>
2024-12-09 10:24:48 +0100

    fået read more function til at fungere

--- src/components/festivalsystem/Accordion.jsx
+++ src/components/festivalsystem/Accordion.jsx
@@ -3,15 +3,15 @@
 import Accordionitem from "./AccordionItem";
 import { useState } from "react";
 
-const Accordion = ({ dimensions, warrantyInformation, shippingInformation, returnPolicy }) => {
+const Accordion = () => {
   const [isOpen, setIsOpen, item] = useState(0);
   return (
     <div>
       <ul className=" list-none w-full">
-        <Accordionitem item={1} isOpen={isOpen} setIsOpen={setIsOpen} question="Hvor rene er camping pladserne?" textOne="bla bla 1" />
-        <Accordionitem item={2} isOpen={isOpen} setIsOpen={setIsOpen} question="Hvor tæt på ligger toilletterne?" textOne="bla bla 2" />
+        <Accordionitem item={1} isOpen={isOpen} setIsOpen={setIsOpen} question="Hvor rene er camping pladserne?" text="bla bla 1" />
+        <Accordionitem item={2} isOpen={isOpen} setIsOpen={setIsOpen} question="Hvor tæt på ligger toilletterne?" text="bla bla 2" />
         <Accordionitem item={3} isOpen={isOpen} setIsOpen={setIsOpen} question="Hvor er madområderne i forhold til campingpladserne?" textOne="bla bla 3" />
-        <Accordionitem item={4} isOpen={isOpen} setIsOpen={setIsOpen} question="Bliver der ryddet op efter mig?" textOne="bla bla 4" />
+        <Accordionitem item={4} isOpen={isOpen} setIsOpen={setIsOpen} question="Bliver der ryddet op efter mig?" text="bla bla 4" />
       </ul>
     </div>
   );

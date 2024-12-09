972f7a3263ccc9900fee894daa4f3f6563ffcd86
Cecilie <cepe0001@stud.kea.dk>
2024-12-09 09:16:58 +0100

    færdig med fetching af data i singleview

--- src/components/festivalsystem/SingleBand.jsx
+++ src/components/festivalsystem/SingleBand.jsx
@@ -1,4 +1,11 @@
 import Image from "next/image";
+import { Caesar_Dressing } from "next/font/google";
+
+const ceasarDressing = Caesar_Dressing({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
 
 const SingleBand = ({ band }) => {
   const { name, members, genre, bio, logo } = band;
@@ -8,15 +15,24 @@ const SingleBand = ({ band }) => {
 
   return (
     <div>
-      <Image src={imageUrl} alt={name} width={250} height={250} />
-      <h2>{name}</h2>
+      <Image src={imageUrl} alt={name} width={450} height={450} />
+      <h2 className={`${ceasarDressing.className} font-bold bg-gradient-to-bl from-customPink to-customOrange bg-clip-text text-transparent text-4xl`}>{name.toUpperCase()}</h2>
+      <div>
+        <h2 className="font-bold">Medlemmer: </h2>
+      </div>
       <ul>
         {band.members.map((member) => (
           <li key={member}>{member}</li>
         ))}
       </ul>
-      <p>{genre}</p>
-      <p>{bio}</p>
+      <div className="flex gap-1">
+        <h2 className="font-bold">Genre:</h2>
+        <p>{genre}</p>
+      </div>
+      <div>
+        <h2 className="font-bold">Beskrivelse:</h2>
+        <p>{bio}</p>
+      </div>
     </div>
   );
 };

e60c157fc04607b6c08e168e2075a8c152cb790b
”sagecave” <Andreassage@gmail.com>
2024-12-09 13:58:02 +0100

    betaling struktur html snart næsten opsat

---
+++ src/components/bookingsystem/BetalingsOplysninger.jsx
@@ -0,0 +1,25 @@
+import CardInfo from ("card-info")
+const BetalingsOplysninger = () => {
+  return (
+    <div>
+      <h1>BETALINGSOPLYSNINGER</h1>
+      <div>
+        <script src="/bower_components/card-info/dist/card-info.min.js"></script>
+        <form action="kortOplysninger">
+          <label htmlFor="kortnummer">Kortnummer</label>
+          <input required placeholder="1234 1234 1234 1234" type="number" />
+          <label htmlFor="kortHolder"> Kortholder</label>
+          <input required placeholder="Kortholder Navn" type="text" />
+          <div>
+            <label htmlFor="udløbsdato">Udløbsdato</label>
+            <input required placeholder="MM/ÅÅ" type="text" />
+            <label htmlFor="Kontrolcifre"> CVC</label>
+            <input required placeholder="CVC" type="number" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default BetalingsOplysninger;

b3403288cb04e5664fde83724c925340ec0cc5bc
Cecilie <cepe0001@stud.kea.dk>
2024-12-10 16:01:28 +0100

    prøver at få stylet filter knapper

--- src/components/festivalsystem/ProgramList.jsx
+++ src/components/festivalsystem/ProgramList.jsx
@@ -46,7 +46,7 @@ function ProgramList({ mergedArray, days }) {
         <div>
           <div className="flex justify-center flex-wrap my-8 mb-20 gap-5">
             {days.map((day) => (
-              <button key={day} className={`border-solid border-customOrange border-[2px] px-4 py-2`} onClick={() => filterActsByDay(day)}>
+              <button key={day} className={`${selectedDay === day} ? bg-customOrange text-white : border-customOrange bg-transparent border-solid border-[2px]`} onClick={() => filterActsByDay(day)}>
                 {day}
               </button>
             ))}

0a20bb891da191148b2c4f27d5c264e3785de22b
Cecilie <cepe0001@stud.kea.dk>
2024-12-11 15:01:39 +0100

    vercel deploy test

---
+++ src/app/program/[slug]/page.js
@@ -0,0 +1,20 @@
+import SingleBand from "@/components/festivalsystem/SingleBand";
+import Header from "@/components/global/Header";
+import Footer from "@/components/global/Footer";
+
+const Band = async ({ params }) => {
+  const slug = (await params).slug;
+  //   let response = await fetch(`http://localhost:8080/bands/${slug}`);
+  let response = await fetch(`https://spring-awesome-stream.glitch.me/bands${slug}`);
+  let data = await response.json();
+
+  return (
+    <section>
+      <Header />
+      <SingleBand band={data} />
+      <Footer />
+    </section>
+  );
+};
+
+export default Band;

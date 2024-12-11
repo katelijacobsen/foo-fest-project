4106e2fd0dc4bbf9f382770ddd50c36cffbc4939
”sagecave” <Andreassage@gmail.com>
2024-12-11 11:28:08 +0100

    paymentFlow mangler kun styling

--- src/components/bookingsystem/PaymentFlow.jsx
+++ src/components/bookingsystem/PaymentFlow.jsx
@@ -24,12 +24,7 @@ const PaymentForm = () => {
   return (
     <div>
       <Cards number={state.number} expiry={state.expiry} cvc={state.cvc} name={state.name} focused={state.focus} />
-      {/* <form>
-        <input type="text" name="number" placeholder="Card Number" value={state.number} onChange={handleInputChange} onFocus={handleInputFocus} aria-label="Indtast dit kortnummer" />
-        <input type="text" name="name" placeholder="Navn" value={state.name} onChange={handleInputChange} onFocus={handleInputFocus} />
-        <input type="text" name="expiry" placeholder="MM/YY" value={state.expiry} onChange={handleInputChange} onFocus={handleInputFocus} />
-        <input type="text" name="cvc" placeholder="CVC" value={state.cvc} onChange={handleInputChange} onFocus={handleInputFocus} />
-      </form> */}
+
       <form action="kortOplysninger">
         <label htmlFor="kortnummer">Kortnummer</label>
         <input name="number" value={state.number} onChange={handleInputChange} onFocus={handleInputFocus} required placeholder="1234 1234 1234 1234" type="number" />

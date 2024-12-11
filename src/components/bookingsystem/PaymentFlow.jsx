3c97c31f8817df2f4e0723cc15e344c6c7b8bc58
”sagecave” <Andreassage@gmail.com>
2024-12-11 09:16:21 +0100

    betaling er næsten færdig mangler bare styling og kortet, som der er problemer med på Paymentflow

--- src/components/bookingsystem/PaymentFlow.jsx
+++ src/components/bookingsystem/PaymentFlow.jsx
@@ -22,47 +22,12 @@ const PaymentForm = () => {
 
   return (
     <div>
-      <Cards
-        number={state.number}
-        expiry={state.expiry}
-        cvc={state.cvc}
-        name={state.name}
-        focused={state.focus}
-      />
+      <Cards number={state.number} expiry={state.expiry} cvc={state.cvc} name={state.name} focused={state.focus} />
       <form>
-        <input
-          type="text"
-          name="number"
-          placeholder="Card Number"
-          value={state.number}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-          aria-label="Indtast dit kortnummer"
-        />
-        <input
-          type="text"
-          name="full-name"
-          placeholder="Navn"
-          value={state.name}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <input
-          type="text"
-          name="expiry"
-          placeholder="MM/YY"
-          value={state.name}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <input
-          type="text"
-          name="cvc"
-          placeholder="CVC"
-          value={state.name}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
+        <input type="text" name="number" placeholder="Card Number" value={state.number} onChange={handleInputChange} onFocus={handleInputFocus} aria-label="Indtast dit kortnummer" />
+        <input type="text" name="full-name" placeholder="Navn" value={state.name} onChange={handleInputChange} onFocus={handleInputFocus} />
+        <input type="text" name="expiry" placeholder="MM/YY" value={state.name} onChange={handleInputChange} onFocus={handleInputFocus} />
+        <input type="text" name="cvc" placeholder="CVC" value={state.name} onChange={handleInputChange} onFocus={handleInputFocus} />
       </form>
     </div>
   );

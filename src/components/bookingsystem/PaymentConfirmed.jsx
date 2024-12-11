ba2e12b6a317e0d737dae41d44c310344fb966ce
Katja Mähleke <katjamaehleke98@gmail.com>
2024-12-11 12:09:52 +0100

    add code

--- src/components/bookingsystem/PaymentConfirmed.jsx
+++ src/components/bookingsystem/PaymentConfirmed.jsx
@@ -1,10 +1,48 @@
 import Form from "next/form";
+import { motion } from "framer-motion";
 
 export default function PaymentConfirmed({ data, formStatus }) {
   return (
     <Form>
       <h2>Ordre Bekræftelse</h2>
-      
+      <motion.div
+        className="svgContainer"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.5 }}
+      >
+        <svg
+          width="150px"
+          height="150px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#4ece69"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <g id="SVGRepo_iconCarrier">
+            <motion.path
+              d="M8.5 12.5L10.5 14.5L15.5 9.5"
+              stroke="#4ece69"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <motion.path
+              d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 5.47087 7.33782 3"
+              stroke="#4ece69"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />
+          </g>
+        </svg>
+      </motion.div>
       <table className="w-full mb-4">
         <thead>
           <tr>
--- src/components/bookingsystem/PaymentFlow.jsx
+++ src/components/bookingsystem/PaymentFlow.jsx
@@ -1,85 +1,23 @@
-
-import "react-credit-cards-2/dist/es/styles-compiled.css";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Cards from "react-credit-cards-2";
-import { useRouter } from "next/navigation";
 
-
-function Payment({formAction, resetForm}) {
-  const [cardInfo, setCardInfo] = useState({
+const PaymentForm = () => {
+  const [state, setState] = useState({
     number: "",
     expiry: "",
     cvc: "",
     name: "",
-    errors: {},
+    focus: "",
   });
-  const router = useRouter();
-  const [timeLeft, setTimeLeft] = useState(30);
 
-  useEffect(() => {
-    if (timeLeft <= 0) {
-      alert("Tiden er udløbet");
-      resetForm(); // Nulstil state til at starte fra step 1
-      formAction(null); //refresh form
-      return;
-    }
-    // Bruger setInterval, så vi kan planlægge at bruge vores callback hver forsinkelse i millisekunder.
-    const timer = setInterval(() => {
-      // Går ned i tiden.
-      setTimeLeft((prevTime) => prevTime - 1);
-    }, 1000); //sætter 1000 som en værdi er bliver erstattet med tiden i useState.
-    //Clean up. clearInterval stopper tiden. Så når den er på 0 skulle den gerne skifte router.
-    return () => clearInterval(timer);
-  }, [timeLeft, resetForm, router]);
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
 
-
-  const handleChange = (e) => {
-    setCardInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setState((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleInputFocus = (evt) => {
-    setCardInfo((prev) => ({ ...prev, focus: evt.target.name }));
-  };
-
-  // Input card expiry onKeyUp handler
-  const handleCardExpiry = (e) => {
-    let expiryDate = e.target.value;
-
-    if (e.keyCode !== 8) {
-      if (expiryDate > 1 && expiryDate.length === 1) {
-        expiryDate = "0" + expiryDate + "/";
-      } else if (expiryDate.length === 2) {
-        expiryDate = expiryDate + "/";
-      }
-
-      setCardInfo({ ...cardInfo, expiry: expiryDate });
-    } else {
-      setCardInfo({ ...cardInfo, expiry: "" });
-    }
-  };
-
-  // Input onKeyDown numbers only handler
-  const handleNumbersOnly = (e) => {
-    let flag;
-
-    if (
-      e.keyCode === 8 ||
-      e.keyCode === 9 ||
-      (e.keyCode === 16 && e.keyCode >= 9) ||
-      e.keyCode === 37 ||
-      e.keyCode === 39 ||
-      e.keyCode === 46 ||
-      (e.keyCode >= 48 && e.keyCode <= 57) ||
-      (e.keyCode >= 96 && e.keyCode <= 105)
-    ) {
-      flag = false;
-    } else {
-      flag = true;
-    }
-
-    if (flag) {
-      e.preventDefault();
-    }
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
   };
 
   return (
@@ -93,6 +31,6 @@ function Payment({formAction, resetForm}) {
       </form>
     </div>
   );
-}
+};
 
-export default Payment;
+export default PaymentForm;

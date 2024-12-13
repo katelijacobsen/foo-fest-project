f6f040bc0ded0792452e94c8bd1b6b3f680221cc
Cecilie <cepe0001@stud.kea.dk>
2024-12-13 11:40:05 +0100

    fjernet frivillig billede

--- src/app/volunteer/page.js
+++ src/app/volunteer/page.js
@@ -4,7 +4,7 @@ import Headline from "@/components/global/Headline";
 import VolunteerRune from "@/img/svg/volunteer_rune.svg";
 import Accordion from "@/components/festivalsystem/Accordion";
 import FaqRune from "@/img/svg/faq_rune.svg";
-import VolunteerImage from "@/img/jpg/volunteer_image.jpg";
+
 import Image from "next/image";
 import RuneCircle from "@/img/svg/rune_circle.svg";
 
@@ -18,7 +18,6 @@ export default async function Page() {
             <Headline src={VolunteerRune} text="BLIV FRIVILLIG" size="text-[4rem] md:text-[5rem]" width={45} height={45} />
             <p className="pt-4 text-lg md:text-xl">Har du lyst til at opleve festivalen på en helt unik måde? Så bliv en del af vores fantastiske hold af frivillige og vær med til at skabe en festival, der bringer nordisk mytologi, musik og fællesskab til live! At være frivillig handler ikke kun om at hjælpe – det handler om at blive en del af en klan, hvor vi sammen bygger noget legendarisk. Vi søger engagerede mennesker, der vil bidrage med deres tid og gode energi, og som samtidig ønsker en oplevelse udover det sædvanlige.</p>
           </div>
-          <Image className="place-self-center" src={VolunteerImage} alt="billede af frivillige i skoven" width={500} height={500} />
         </div>
         <div className="grid md:grid-cols-2 h-screen">
           <div className="grid gap-4 items-center">

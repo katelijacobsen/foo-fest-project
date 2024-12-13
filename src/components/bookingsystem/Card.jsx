227a7c6f11bbdf73ad6f659a2a8dd0fa530fab35
”sagecave” <Andreassage@gmail.com>
2024-12-13 10:52:45 +0100

    merge fix betalings kort

--- src/components/bookingsystem/Card.jsx
+++ src/components/bookingsystem/Card.jsx
@@ -1,22 +1,8 @@
 import CounterInput from "./CounterInput";
-<<<<<<< HEAD
-import { useState } from "react";
-import PrimaryButton from "@/components/global/buttonFolder/PrimaryButton";
-
-export default function Card({ formAction, inputName, title, price, valuta, className }) {
-=======
 import { useContext, useState } from "react";
 import { CartContext } from "@/app/tickets/page";
 
-export default function Card({
-  formAction,
-  ticketType,
-  title,
-  price,
-  valuta,
-  className,
-}) {
->>>>>>> Bookingsystem
+export default function Card({ formAction, ticketType, title, price, valuta, className }) {
   const [count, setCount] = useState(0);
   const setCart = useContext(CartContext);
 
@@ -27,8 +13,8 @@ export default function Card({
 
   const updateCount = (count) => {
     setCart((prev) => {
-      const single = ticketType === 'single' ? count : prev.tickets.single;
-      const vip = ticketType === 'vip' ? count : prev.tickets.vip;
+      const single = ticketType === "single" ? count : prev.tickets.single;
+      const vip = ticketType === "vip" ? count : prev.tickets.vip;
       return { ...prev, tickets: { single: single, vip: vip } };
     });
     setCount(count);
@@ -39,29 +25,11 @@ export default function Card({
       <h2 className="text-3xl font-bold ">{title}</h2>
       <p className="text-2xl font-bold">{price}</p>
       <p className="font-bold">{valuta}</p>
-<<<<<<< HEAD
-      <CounterInput name={inputName} max={10} count={count} setCount={setCount} />
-=======
-      <CounterInput
-        name={ticketType === "single" ? "singleTickets" : "vipTickets"}
-        max={10}
-        count={count}
-        setCount={updateCount}
-      />
-      <button
-         className={`m-4 p-5 ${
-          count === 0
-            ? "bg-gray-300 text-gray-500 cursor-not-allowed"
-            : "bg-yellow-400 text-black hover:bg-yellow-500"
-        }`}
-        type="submit"
-        formAction={handleBuy}
-        disabled={count === 0}
-      >
+      <CounterInput name={ticketType === "single" ? "singleTickets" : "vipTickets"} max={10} count={count} setCount={updateCount} />
+      <button className={`m-4 p-5 ${count === 0 ? "bg-gray-300 text-gray-500 cursor-not-allowed" : "bg-yellow-400 text-black hover:bg-yellow-500"}`} type="submit" formAction={handleBuy} disabled={count === 0}>
         Køb
       </button>
       {/* <SecondButton buttonContent={"Køb"} type="submit" formAction=""/> */}
->>>>>>> Bookingsystem
     </div>
   );
 }
--- src/components/bookingsystem/ChooseTicket.jsx
+++ src/components/bookingsystem/ChooseTicket.jsx
@@ -1,9 +1,5 @@
 import Card from "./Card";
-<<<<<<< HEAD
-import PrimaryButton from "@/components/global/buttonFolder/PrimaryButton";
-=======
 import { useContext } from "react";
->>>>>>> Bookingsystem
 
 export default function ChooseTicket({ formAction }) {
   const handleBuy = (e) => {
@@ -12,28 +8,9 @@ export default function ChooseTicket({ formAction }) {
   };
 
   return (
-<<<<<<< HEAD
-    <form action={handleBuy}>
-      <Card inputName={`singleTickets`} formAction={formAction} title="Enkel Billet" price="799" valuta="DKK + Fee" />
-      <Card inputName={`vipTickets`} formAction={formAction} title="VIP Billet" price="1299" valuta="DKK + Fee" />
-      <PrimaryButton color="bg-gradient-to-r from-[#ec2783] from-12% via-[#d82023] via-46% to-[#ec4d08] to-87%" buttonContent="Køb" type="submit"></PrimaryButton>
-=======
     <form>
-      <Card
-        ticketType={`single`}
-        formAction={formAction}
-        title="Enkel Billet"
-        price="799"
-        valuta="DKK + Fee"
-        />
-      <Card
-        ticketType={`vip`}
-        formAction={formAction}
-        title="VIP Billet"
-        price="1299"
-        valuta="DKK + Fee"
-      />
->>>>>>> Bookingsystem
+      <Card ticketType={`single`} formAction={formAction} title="Enkel Billet" price="799" valuta="DKK + Fee" />
+      <Card ticketType={`vip`} formAction={formAction} title="VIP Billet" price="1299" valuta="DKK + Fee" />
     </form>
   );
 }

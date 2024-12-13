62f477c750b7221708d65590ffe9e4eef7ee5aa0
Cecilie <cepe0001@stud.kea.dk>
2024-12-13 08:59:49 +0100

    stylet camping section på forside

--- src/components/festivalsystem/Camping.jsx
+++ src/components/festivalsystem/Camping.jsx
@@ -7,10 +7,10 @@ const Camping = ({ text }) => {
   return (
     <section className="py-6 px-4 h-screen grid place-content-center md:grid-cols-2 gap-4 max-w-screen-xl mx-auto items-start">
       <div>
-        <Headline src={CampingRune} text="CAMPING" />
-        <p className="pt-6">{text}</p>
+        <Headline src={CampingRune} width={45} height={45} size="text-4xl md:text-6xl" text="CAMPING" />
+        <p className="pt-6 text-lg md:text-lg">{text}</p>
       </div>
-      <Image className="justify-self-center" src={CampingMap} alt="illustration af campingpladser" width={500} height={500} />
+      <Image className="justify-self-center" src={CampingMap} alt="illustration af campingpladser" width={650} height={650} />
     </section>
   );
 };

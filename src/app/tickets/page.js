eac8630235d58995cc317df66700c50ad2d75cfc
katelijacobsen <katjamaehleke98@gmail.com>
2024-12-16 14:02:40 +0100

    add notes

--- src/app/tickets/page.js
+++ src/app/tickets/page.js
@@ -16,7 +16,7 @@ const ceasarDressing = Caesar_Dressing({
   display: "swap",
 });
 //=================//
-// constant variabel hvor vi definere indholdet når ikke noget er blevet valgt. Variablen bruger vi til at refreshe vores 
+// constant variabel hvor vi definere indholdet når ikke noget er blevet valgt. Variablen bruger vi til at refreshe vores
 // Multistep-form (f.eks. hvis brugeren er gået over de 5min i PaymentFlow).
 //=================//
 const defaultState = {
@@ -42,18 +42,29 @@ const defaultState = {
     cvc: "",
   },
 };
-// Funktion ved navn handleStep med prev (previousState & formData parametre). Begge parameter skal vi bruge både i 
+
+//=================//
+// Funktion ved navn handleStep med prev (previousState & formData parametre). Begge parameter skal vi bruge både i
 // funktionen & sende den vider til children-komponenterne.
+//=================//
+
 const handleStep = (prev, formData) => {
+  //=================//
   // Hvis formData er lige med 0 så vis vores defaultState (vores konstant ovenpå)
+  //=================//
+
   if (formData === null) {
     return defaultState;
   }
   const step = formData.get("stepBack") ? -1 : +1;
-// Hvis vores previous step er lig med 0 så gå vider til det næste skridt.
-// formatér vores valg af billettr og  brug get() metoden til at få dataen med som numbers ("+" foran formData ).
-// Er det uden "+" er det en String (se i konsollen hvad type dataer vpres objekt spytter ud).
-// vi bruger spread syntaksen (...prev) så vi kan stadig få vores data med, der opdateres i baggrunden.
+
+  //=================//
+  // Hvis vores previous step er lig med 0 så gå vider til det næste skridt.
+  // formatér vores valg af billettr og  brug get() metoden til at få dataen med som numbers ("+" foran formData ).
+  // Er det uden "+" er det en String (se i konsollen hvad type dataer vpres objekt spytter ud).
+  // vi bruger spread syntaksen (...prev) så vi kan stadig få vores data med, der opdateres i baggrunden.
+  //=================//
+
   if (prev.step === 0) {
     return {
       ...prev,
@@ -66,7 +77,11 @@ const handleStep = (prev, formData) => {
   }
   if (prev.step === 1) {
     console.log(formData.get("campsite"));
-// Samme princip som step 0, hvor vi derudover anvender en boolean til vores greenCamping, hvis brugeren tilføjer det.
+
+    //=================//
+    // Samme princip som step 0, hvor vi derudover anvender en boolean til vores greenCamping, hvis brugeren tilføjer det.
+    //=================//
+
     return {
       ...prev,
       step: prev.step + step,
@@ -80,11 +95,19 @@ const handleStep = (prev, formData) => {
   }
 
   if (prev.step === 2) {
-    //Vi laver en liste over vores billetter med Array (statisk metode, som laver en ny liste ud fra vores data) 
+
+    //=================//
+    //Vi laver en liste over vores billetter med Array (statisk metode, som laver en ny liste ud fra vores data)
     // & ved at loop igennem antallet af billetter brugeren valgt i begyndelsen.
+    //=================// 
+
     const singleGuests = Array.from(
       { length: prev.tickets.single },
-      //giver den unikke id'er ( _ & i (i står for index)), så man kan finde dem i vores formData
+
+      //=================//
+      //giver den unikke id'er ( _ & i (i står for index)), så man kan finde dem i vores formData.
+      //=================//
+
       (_, i) => ({
         firstName: formData.get(`single_firstName_${i}`),
         lastName: formData.get(`single_lastName_${i}`),
@@ -124,6 +147,8 @@ const handleStep = (prev, formData) => {
   }
 };
 // Deler data uden at propdril fra Page. Så kan vi for adgang til vores data.
+// Opretter en standardværdier til vores indkøbskurv. Den er på 0 indtil brugeren tilføjer noget.
+// Vi siger bare her at CartContext er navnet på vores kontekst. Så dataen bliver delt op tværs af vores komponenter.
 export const CartContext = createContext(null);
 
 export default function Page() {
@@ -140,12 +165,15 @@ export default function Page() {
       greenCamping: false,
     },
   };
-// Forskellige Hooks der bliver anvendt i vores parent-komponent, så vi også kan sende dem vider til vores children (så de kan snak med hinanden)
-//
-
+  // Forskellige Hooks der bliver anvendt i vores parent-komponent, så vi også kan sende dem vider til vores children (så de kan snak med hinanden)
+  // cart er vores værdi vi sender vider i vores komponenter, mens setCart er vores funktion til at opdatere vores cart-variabel.
+  // Vi giver vores useState hook 'defaultCart' som standartværdi (tom kurv).
   const [cart, setCart] = useState(defaultCart);
+  // Bruger useActionState-hook til at håndtere vores data-tilstand. state er vores variabel, der bliver opdateret for hver brugeren handling
+  // der sker i hver komponent.
+  // F.eks. har vi givet state vider til campsite. Ligesom en filmappe struktur prøver den at finde antal af billetter vi tidliger har
+  // valgt, og bruger det samme antal telte-billetter brugern må bruge.
   const [state, formAction] = useActionState(handleStep, defaultState);
-  const formStatus = useFormStatus();
 
   console.log(state);
   return (
@@ -167,13 +195,16 @@ export default function Page() {
               <Campsite state={state} formAction={formAction} />
             )}
             {state.step === 2 && (
-              <ContactInfo state={state} tickets={state.tickets} formAction={formAction} />
+              <ContactInfo
+                state={state}
+                tickets={state.tickets}
+                formAction={formAction}
+              />
             )}
             {state.step === 3 && <PaymentFlow formAction={formAction} />}
             {state.step === 4 && (
               <PaymentComfirmed
                 state={state}
-                formStatus={formStatus}
                 startDraw={true}
               />
             )}

0de2b9a707df20488d1958710a5ae12b94a221db
Cecilie <cepe0001@stud.kea.dk>
2024-12-18 10:17:12 +0100

    slettet udkommenteret kommentarer i supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,3 @@
-// import { headers } from "next/headers";
-
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const apikey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
@@ -25,14 +23,3 @@ export async function postSub(postData) {
     body: JSON.stringify(postData),
   });
 }
-
-// export async function postSub(postData) {
-//   const response = await fetch(url, {
-//     method: "POST",
-//     headers: headersList,
-//     body: JSON.stringify(postData),
-//   });
-
-//   const data = await response.json();
-//   return data;
-// }

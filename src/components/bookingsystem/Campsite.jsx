5496039de6d34205675d031e6ad129faa12442bd
”sagecave” <Andreassage@gmail.com>
2024-12-18 12:02:55 +0100

    s

--- src/components/bookingsystem/Campsite.jsx
+++ src/components/bookingsystem/Campsite.jsx
@@ -22,6 +22,7 @@ export default function Campsite({ state, formAction }) {
   const [greenCamping, setGreenCamping] = useState(false);
   const [countError, setCountError] = useState("");
   const [handleError, setHandleError] = useState("");
+  const [spotHandler, setSpotHandler] = useState(0);
 
   const [data, setData] = useState([]);
 
@@ -103,15 +104,8 @@ export default function Campsite({ state, formAction }) {
 
   return (
     <div className="flex justify-center mx-4">
-      <motion.form
-        initial={{ opacity: 0, x: 20 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.3 }}
-        className=" inline-flex flex-col flex-1 bg-gradient-to-tl border border-gray-500 bg-customBlack_5 p-4 my-4 rounded-md"
-      >
-        <h2 className={`${ceasarDressing.className} text-3xl text-white`}>
-          HVOR VIL DU CAMPE?
-        </h2>
+      <motion.form initial={{ opacity: 0, x: 20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.3 }} className=" inline-flex flex-col flex-1 bg-gradient-to-tl border border-gray-500 bg-customBlack_5 p-4 my-4 rounded-md">
+        <h2 className={`${ceasarDressing.className} text-3xl text-white`}>HVOR VIL DU CAMPE?</h2>
         <h3 className="font-bold text-sm mb-4 text-gray-400">VÆLG VENLIGST EN LEDIG CAMPINGOMRÅDE</h3>
         {isLoading && (
           <div className="flex justify-center items-center">
@@ -123,17 +117,11 @@ export default function Campsite({ state, formAction }) {
             <li
               onClick={() => updateCampsite(spot.area)}
               key={i}
-              className={`${
-                spot.available < numPeople &&
-                "bg-gray-300 text-gray-500 cursor-not-allowed hidden disabled"
-              }${
+              className={`${spot.available < numPeople && "bg-gray-300 text-gray-500 cursor-not-allowed hidden disabled"}${
                 spot.area === selectedCampsite && "border-4"
               } bg-gradient-to-tl border border-gray-900 from-customBlack_2 to-customBlack p-2 rounded-md select-none cursor-pointer`}
               style={{
-                borderImage:
-                  spot.area === selectedCampsite
-                    ? "linear-gradient(to right, #EC2783, #D82023 , #EC4D08) 1"
-                    : "none",
+                borderImage: spot.area === selectedCampsite ? "linear-gradient(to right, #EC2783, #D82023 , #EC4D08) 1" : "none",
                 borderRadius: "0.375rem",
                 borderWidth: spot.area === selectedCampsite ? "2px" : "1px",
               }}
@@ -146,34 +134,21 @@ export default function Campsite({ state, formAction }) {
 
         <div className="flex flex-col justify-evenly gap-4">
           <section>
-            <h4
-              className={`${ceasarDressing.className} text-3xl text-white mt-8`}
-            >
-              LEJE AF TELTE
-            </h4>
+            <h4 className={`${ceasarDressing.className} text-3xl text-white mt-8`}>LEJE AF TELTE</h4>
             <ul className="my-4 inline-flex flex-col gap-6">
               <li className=" flex-row text-white flex gap-12 bg-gradient-to-tl border border-gray-900 from-customBlack_2 to-customBlack p-4 rounded-md">
                 <div>
                   <h3 className="font-bold text-xl">2 Personers Telt</h3>
                   <p className="text-xs font-normal text-gray-300">299kr</p>
                 </div>
-                <CounterInput
-                  name="twoPeople"
-                  count={twoPersonCount}
-                  setCount={updateTwoPersonTentCount}
-                />
+                <CounterInput name="twoPeople" count={twoPersonCount} setCount={updateTwoPersonTentCount} />
               </li>
               <li className="flex flex-row text-white gap-12  bg-gradient-to-tl border border-gray-900 from-customBlack_2 to-customBlack p-4 rounded-md">
                 <div>
                   <h3 className="font-bold text-xl">3 Personers Telt</h3>
                   <p className="text-xs font-normal text-gray-300">399kr</p>
                 </div>
-                <CounterInput
-                  name="threePeople"
-                  max={10}
-                  count={threePersonCount}
-                  setCount={updateThreePersonTentCount}
-                />
+                <CounterInput name="threePeople" max={10} count={threePersonCount} setCount={updateThreePersonTentCount} />
               </li>
             </ul>
             {countError && (
@@ -183,11 +158,7 @@ export default function Campsite({ state, formAction }) {
             )}
           </section>
           <section>
-            <h3
-              className={`${ceasarDressing.className} text-3xl text-white mb-4`}
-            >
-              SUPPLEMENT
-            </h3>
+            <h3 className={`${ceasarDressing.className} text-3xl text-white mb-4`}>SUPPLEMENT</h3>
 
             <div className="flex items-center p-4 rounded-md bg-gradient-to-tl border border-green-800 from-black to-green-900">
               <div className="flex h-5">
@@ -202,22 +173,13 @@ export default function Campsite({ state, formAction }) {
                 />
               </div>
               <div className="ms-2 text-sm">
-                <label
-                  htmlFor="helper-checkbox"
-                  className="font-bold inline-flex gap-2 items-baseline text-xl text-white"
-                >
+                <label htmlFor="helper-checkbox" className="font-bold inline-flex gap-2 items-baseline text-xl text-white">
                   Grøn Camping <FaLeaf className="text-green-400" />
                 </label>
-                <p
-                  id="helper-checkbox-text"
-                  className="text-xs font-normal text-gray-300"
-                >
+                <p id="helper-checkbox-text" className="text-xs font-normal text-gray-300">
                   249kr
                 </p>
-                <p className="font-light text-sm text-gray-100 w-64">
-                  Perfekt til dig, der ønsker en mere miljøvenlig
-                  festivaloplevelse.
-                </p>
+                <p className="font-light text-sm text-gray-100 w-64">Perfekt til dig, der ønsker en mere miljøvenlig festivaloplevelse.</p>
               </div>
             </div>
           </section>
